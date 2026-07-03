d393604c8dc6835c875abaf6e0f11978e21f6eaa
oanastirban <oanasabrinastirban@gmail.com>
2026-07-03 22:07:25 +0200

    implementazione lab

--- dictionary.py
+++ dictionary.py
@@ -1,42 +1,12 @@
-class Dictionary:
-    def __init__(self):
-        self.dict = {}
+class Parola:
+    def __init__(self, parola,traduzione):
+        self.parola = parola
+        self.traduzioni = [traduzione]
+    def __repr__(self):
+        return f"{self.parola} {self.traduzioni}"
 
-    def addWord(self, alien_word, translations):
-        alien_word = alien_word.lower()
 
-        if not isinstance(translations, list):
-            translations = [translations]
 
-        if alien_word not in self.dict:
-            self.dict[alien_word] = []
 
-        for t in translations:
-            t_lower = t.lower()
-            if t_lower not in self.dict[alien_word]:
-                self.dict[alien_word].append(t_lower)
 
-    def translate(self, alien_word):
-        alien_word = alien_word.lower()
-        return self.dict.get(alien_word, [])
-
-    def translateWordWildCard(self, wildcard_word):
-        wildcard_word = wildcard_word.lower()
-        matches = {}
-
-        for word, translations in self.dict.items():
-            if len(word) == len(wildcard_word):
-                match = True
-                for i in range(len(word)):
-                    if wildcard_word[i] != '?' and wildcard_word[i] != word[i]:
-                        match = False
-                        break
-                if match:
-                    matches[word] = translations
-
-        return matches
-
-    def getAll(self):
-
-        return self.dict
 
--- main.py
+++ main.py
@@ -1,44 +1,95 @@
-import translator as tr
+import dictionary
+import re
 
-t = tr.Translator()
+dizionario: list[dictionary.Parola] = []
 
+with open("dictionary.txt", "r") as f:
+    for line in f:
+        l = line.strip().split(" ")
+        parola = dictionary.Parola(l[0], l[1])
+        if len(l) > 2:
+            parola.traduzioni.extend(l[2:])
+        dizionario.append(parola)
 
-t.loadDictionary("dictionary.txt")
+menuText = """
+-------------------------
+Translator Alien-Italian
+-------------------------
+1. Aggiungi nuova parola
+2. Cerca una traduzione
+3. Cerca con wildcard
+4. Stampa tutto il Dizionario
+5. Exit
+-------------------------
+"""
 
-while(True):
+while True:
+    print(menuText)
 
-    t.printMenu()
+    input_uomo = input()
+    if not input_uomo.isdigit():
+        print("Inserisci un numero")
+        exit(1)
+    input_uomo = int(input_uomo)
+    if input_uomo > 5 or input_uomo < 1:
+        print("Inserisci un numero valido")
 
-    t.loadDictionary("dictionary.txt")
+    if input_uomo == 1:
 
-    txtIn = input("> ")
-    if not txtIn.isdigit():
-        print("Errore: devi inserire un numero.")
-        continue
+        input_parola = input("Aggiungi nuova parola e la sua traduzione: ")
+        i = input_parola.lower().strip().split(" ")
 
-    scelta = int(txtIn)
-    # Add input control here!
+        if len(i) == 2:
+            p = i[0].isalpha()
+            t = i[1].isalpha()
+            if p == True and t == True:
+                for parola in dizionario:
+                    if parola.parola == i[0]:
+                        parola.traduzioni.append(i[1])
+                        break
+                else:
+                    parola = dictionary.Parola(i[0], i[1])
+                    dizionario.append(parola)
+        if len(i) > 2:
+            p = i[0].isalpha()
+            t = all(map(lambda x: x.isalpha(), i[1:]))
+            if p == True and t == True:
+                for parola in dizionario:
+                    if parola.parola == i[0]:
+                        parola.traduzioni.extend(i[1:])
+                        break
+                else:
+                    parola = dictionary.Parola(i[0], i[1])
+                    parola.traduzioni.extend(i[2:])
+                    dizionario.append(parola)
 
-    if int(txtIn) == 1:
-        print("Ok, quale parola devo aggiungere? (es. parola_aliena traduzione)")
-        txtIn = input("> ")
+    if input_uomo == 2:
+        input_parola = input("Cerca traduzione: ")
+        i = input_parola.lower().strip()
+        if i.isalpha():
+            for t in dizionario:
+                if t.parola == i:
+                    print(" ".join(t.traduzioni))
+                    break
+            else:
+                print("Parola non trovata")
 
-        entry = txtIn.strip().split()
-        t.handleAdd(entry)
-        pass
-    if int(txtIn) == 2:
-        print("Ok, quale parola devo cercare?")
-        txtIn = input("> ")
+    if input_uomo == 3:  # Usiamo regex per semplificarci la vita, il . Indica todas le lettere
+        # la funzione regex funziona anche per più di un punto ?
+        input_wild = input("inserisci parola da cercare (max 1 ?): ")
+        new = input_wild.replace("?", ".")
+        i = new.lower().strip()
+        for parola in dizionario:
+            if re.match(i, parola.parola):
+                print(" ".join(parola.traduzioni))
 
-        t.handleTranslate(txtIn.strip())
-    if int(txtIn) == 3:
-        print("Ok, quale parola devo cercare con wildcard?")
-        txtIn = input("> ")
-        t.handleWildCard(txtIn.strip())
+    if input_uomo == 4:
+        for line in dizionario:
+            print(f"{line.parola} {" ".join(line.traduzioni)}")
 
-    if int(txtIn) == 4:
-        t.handlePrintAll()
+    if input_uomo == 5:
+        with open(r"dictionary.txt", "w") as f:
+            for i in dizionario:
+                f.write(f"{i.parola} {" ".join(i.traduzioni)} \n")
 
-    if scelta == 5:
-        print("Chiusura del programma in corso...")
-        break
+        exit(0)
